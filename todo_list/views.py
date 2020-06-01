32fd2a8d9e13596ab7600ee80d7957ff7e52b618
minopanda <mino_panda3412@yahoo.co.jp>
2020-06-01 15:37:30 +0900

    [add]success message

--- todo_list/views.py
+++ todo_list/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import List
 from .forms import ListForm
+from django.contrib import messages
 # Create your views here.
 
 def home(request):
@@ -11,7 +12,9 @@ def home(request):
         if form.is_valid():
             form.save()
             all_items = List.objects.all
+            messages.success(request, ('Item Has Been Added To List'))
             return render(request, 'home.html', {'all_items': all_items})
+            
     else:
         all_items = List.objects.all
         return render(request, 'home.html', {'all_items': all_items})
